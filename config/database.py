eee74666e5665aa66ab57e0e1394c93ae3fc2b28
hady-awayda <hady.awayda@gmail.com>
2024-08-11 13:02:39 +0300

    Updated database connection string

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://username:password@localhost/dbname"
+DATABASE_URL = "mysql+pymysql://root:sefactory@3.146.255.194/hackathon-dev"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
